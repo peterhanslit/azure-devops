ce3aeac889f4aca3c4587d8f6e0402530e6d5b63
Philip Peterhansl <philip@peterhansl.de>
2018-09-24 23:39:21 +0200

    Create ruby-installer.sh
    Taken from https://gorails.com/setup/ubuntu/18.04, using recommended rbenv-variant

---
+++ ruby-installer.sh
@@ -0,0 +1,13 @@
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.3.7
+rbenv global 2.3.7
+ruby -v
